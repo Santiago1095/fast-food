9d5e11ab585f0d95147d49b06e108d83d1cae22a
Santiago <santiago.mbaron@gmail.com>
2024-08-06 05:02:52 -0500

    calls and whatsapp

--- src/components/Restaurants/ElBuenGusto.js
+++ src/components/Restaurants/ElBuenGusto.js
@@ -29,14 +29,43 @@ export default function ElBuenGusto() {
 
       <div>
         <h2 className="text-dark text-center fw-bold py-2">
-          Realiza tus pedidos al 321 367 9910 o escribenos al whatsapp
-          <Button href="https://wa.link/olzdv2" className="bg-success">
-            {" "}
-            <SiWhatsapp />{" "}
-
+          Realiza tus pedidos al numero
+          <a className="" href="tel:+57 3213679910">
+            {" 321 367 9910 "}
+          </a>
+          o escribenos al
+          <a href="https://wa.link/olzdv2" className="">
+            {" Whatsapp "}
             
-          </Button>
+          </a>
         </h2>
+
+        <Button
+      variant="outline-light"
+      className="border border-0 py-1"
+      style={{
+        position: "fixed",
+        bottom: 25,
+        right: 10,
+        zIndex: 999,
+        zoom: 1.5,
+        borderRadius: 50,
+        padding: 6,
+      }}
+    >
+      <a href="https://wa.link/olzdv2" target="_blank" rel="noopener noreferrer" className="text-white">
+        <SiWhatsapp
+          className="bg-success py-2"
+          style={{
+            width: "auto",
+            height: "auto",
+            justifyContent: "center",
+            borderRadius: 50,
+            padding: 7,
+          }}
+        />
+      </a>
+    </Button>
       </div>
 
       <Container className="py-1  bg-primary-subtle rounded-4">
@@ -402,11 +431,14 @@ export default function ElBuenGusto() {
             </Col>
           </Row>
         </CardGroup>
+        <CardTitle tag="h1" className="text-center fw-bold">
+          En donde estamos ubicados
+        </CardTitle>
         <div className="py-2">
           <div className="ratio ratio-21x9 ">
             <iframe
               className="rounded-3"
-              src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3139.932827943921!2d-72.369625!3d5.299308!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNcKwMTcnNTcuNSJOIDcywrAyMicxMC43Ilc!5e1!3m2!1ses-419!2sco!4v1713597139838!5m2!1ses-419!2sco"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d275.77946446624827!2d-72.40646979254676!3d5.335859216787204!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e6b0dca93add7cb%3A0x46dfbc4a24770cfe!2sYopal%2C%20Casanare!5e1!3m2!1ses!2sco!4v1722934445055!5m2!1ses!2scohttps://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d993.1322254067773!2d-72.40713117218571!3d5.335906629025571!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e6b0dc91ed7b7cf%3A0x620235eb068a9f46!2sCra.%208b%20%2318-2%20a%2018-92%2C%20Yopal%2C%20Casanare!5e0!3m2!1ses!2sco!4v1722934723278!5m2!1ses!2scohttps://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d945.2860114223361!2d-72.40713117218571!3d5.335906629025571!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e6b0dc91ed7b7cf%3A0x620235eb068a9f46!2sCra.%208b%20%2318-2%20a%2018-92%2C%20Yopal%2C%20Casanare!5e1!3m2!1ses!2sco!4v1722934829469!5m2!1ses!2sco"
               title="Restaurante El Buen Gusto"
               allowfullscreen
             ></iframe>
